33bd06fd548780dfbf8f3d3239a362207ba9e745
michaelwilliams1 <mike.williams@avanade.com>
2023-11-11 15:26:18 -0800

    Check if Chocolatey is already installed

--- Set-Chocolatey.ps1
+++ Set-Chocolatey.ps1
@@ -1,13 +1,23 @@
-# Set directory for installation - Chocolatey does not lock
-# down the directory if not the default
-$InstallDir='C:\ProgramData\chocoportable'
-$env:ChocolateyInstall="$InstallDir"
+if($env:ChocolateyInstall -ne $null -and (Test-Path "$env:ChocolateyInstall\choco.exe")) 
+{
+  Write-Output "Chocolatey is already installed"
+}
+else 
+{
+  # Set directory for installation - Chocolatey does not lock
+  # down the directory if not the default
+  $InstallDir='C:\ProgramData\chocoportable'
+  $env:ChocolateyInstall="$InstallDir"
 
-# If your PowerShell Execution policy is restrictive, you may
-# not be able to get around that. Try setting your session to
-# Bypass.
-Set-ExecutionPolicy Bypass -Scope Process -Force;
+  # If your PowerShell Execution policy is restrictive, you may
+  # not be able to get around that. Try setting your session to
+  # Bypass.
+  Set-ExecutionPolicy Bypass -Scope Process -Force;
 
-# All install options - offline, proxy, etc at
-# https://chocolatey.org/install
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  # All install options - offline, proxy, etc at
+  # https://chocolatey.org/install
+  Write-Output "Installing Chocolatey..."
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  Write-Output "Chocolatey Installed"
+}
+
